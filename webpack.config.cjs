e0dbc50bfd19cc62a186c5d74372a8d3b1b890eb
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-06 14:30:53 -0400

    added a couple more babel transforms to build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -20,7 +20,16 @@ module.exports = {
       {
         test: /\.(ts|js)x?$/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            "presets": [
+              "@babel/preset-env"
+            ],
+            "plugins": [
+              "@babel/plugin-proposal-nullish-coalescing-operator",
+              "@babel/plugin-proposal-optional-chaining"
+            ]
+          }
         }
       }
     ].filter(Boolean)
