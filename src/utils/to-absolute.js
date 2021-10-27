11a83f782af6dd5b85c34f6eac26c6e08685ad95
DanielJDufour <daniel.j.dufour@gmail.com>
2021-10-26 23:02:43 -0400

    ran format on a few more utility files

--- src/utils/to-absolute.js
+++ src/utils/to-absolute.js
@@ -1,8 +1,8 @@
-import URI from 'urijs';
+import URI from "urijs";
 
 import isRelative from "./is-relative.js";
 
-export default function toAbsolute (href, baseUrl, stringify = true) {
+export default function toAbsolute(href, baseUrl, stringify = true) {
   let uri = URI(href);
   if (isRelative(uri)) {
     uri = uri.absoluteTo(baseUrl);
