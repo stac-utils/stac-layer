c51ce460b5d22de6ba910d2b51f541c740c5a005
DanielJDufour <daniel.j.dufour@gmail.com>
2021-10-26 23:05:18 -0400

    added timeout to georaster layer creation

---
+++ src/utils/create-georaster-layer.js
@@ -0,0 +1,14 @@
+import parseGeoRaster from "georaster";
+import GeoRasterLayer from "georaster-layer-for-leaflet";
+import withTimeout from "./with-timeout";
+
+export default function createGeoRasterLayer(url, options) {
+  return withTimeout(5 * 1000, async () => {
+    const georaster = await parseGeoRaster(url);
+
+    // just in case
+    if (options.debugLevel < 0) options.debugLevel = 0;
+
+    return new GeoRasterLayer({ georaster, ...options });
+  });
+}
