c7d03e22f0499b863049fc12bc63b1d2cfeb4fe8
DanielJDufour <daniel.j.dufour@gmail.com>
2021-10-28 17:34:12 -0400

    fixed merge conflicts

--- src/utils/is-bounding-box.js
+++ src/utils/is-bounding-box.js
@@ -1,7 +1,13 @@
-export default function isBoundingBox (bbox) {
-  return Array.isArray(bbox) &&
-  bbox.length === 4 &&
-  bbox.every(n => typeof n === "number") &&
-  bbox[0] < bbox[2] && bbox[1] < bbox[3] &&
-  bbox[0] >= -180 && bbox[1] >= -90 && bbox[2] <= 180 && bbox[3] <= 90;
+export default function isBoundingBox(bbox) {
+  return (
+    Array.isArray(bbox) &&
+    bbox.length === 4 &&
+    bbox.every(n => typeof n === "number") &&
+    bbox[0] < bbox[2] &&
+    bbox[1] < bbox[3] &&
+    bbox[0] >= -180 &&
+    bbox[1] >= -90 &&
+    bbox[2] <= 180 &&
+    bbox[3] <= 90
+  );
 }
