6bb972d66d37fc4470c59b7af1e28e48712e3cc7
DanielJDufour <daniel.j.dufour@gmail.com>
2023-02-28 19:16:47 -0500

    added parse-alphas

---
+++ src/utils/parse-alphas.js
@@ -0,0 +1,39 @@
+import range from "./range.js";
+
+export default async function parseAlphas(georaster) {
+  const geotiff = georaster._geotiff;
+  const image = await geotiff.getImage();
+
+  const { BitsPerSample, ExtraSamples, SampleFormat } = image.fileDirectory;
+  if (!SampleFormat) SampleFormat = new Array(BitsPerSample.length).fill(1);
+
+  const bands = range(BitsPerSample.length).map(i => {
+    const sampleFormat = SampleFormat[i];
+    const nbits = BitsPerSample[i];
+
+    let int, min, range;
+    if (sampleFormat === 1) {
+      // unsigned integer data
+      int = true;
+      min = 0;
+      const max = Math.pow(2, nbits) - 1;
+      range = max - min;
+    } else if (sampleFormat === 2) {
+      // two's complement signed integer data
+      isInteger = true;
+      min = -1 * Math.pow(2, nbits - 1);
+      const max = Math.pow(2, nbits - 1) - 1;
+      range = max - min;
+    } else if (sampleFormat === 3) {
+      // IEEE floating point data
+      isInteger = false;
+    } else if (SampleFormat === 4) {
+      // undefined data format
+    }
+
+    return [i, { int, min, range }];
+  });
+  const alphas = bands.slice(bands.length - ExtraSamples.length);
+
+  return Object.fromEntries(alphas);
+}
