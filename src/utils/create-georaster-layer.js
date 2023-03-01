04a43098749ac189fedc303a3ba89ed5fa3fe908
DanielJDufour <daniel.j.dufour@gmail.com>
2023-02-28 19:17:20 -0500

    added enhanced epsg code parsing to create-georaster-layer.js

--- src/utils/create-georaster-layer.js
+++ src/utils/create-georaster-layer.js
@@ -1,5 +1,6 @@
 import parseGeoRaster from "georaster";
 import GeoRasterLayer from "georaster-layer-for-leaflet";
+import get_epsg_code from "geotiff-epsg-code";
 import withTimeout from "./with-timeout.js";
 
 export default function createGeoRasterLayer(url, options) {
@@ -8,6 +9,12 @@ export default function createGeoRasterLayer(url, options) {
 
     // just in case
     if (options.debugLevel < 0) options.debugLevel = 0;
+
+    if ([undefined, null, "", 32767].includes(georaster.projection)) {
+      if (georaster._geotiff) {
+        georaster.projection = await get_epsg_code(georaster._geotiff);
+      }
+    }
 
     const layer = new GeoRasterLayer({ georaster, ...options });
 
